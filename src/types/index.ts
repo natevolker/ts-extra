d7b4abecfcca88ded12e6658981753a86e858b8e
Nate Volker <natevolker@gmail.com>
2022-01-24 15:50:47 -0600

    fix: adding AnyMap

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 export type { AnyArray } from "./any-array";
 export type { AnyFunction } from "./any-function";
+export type { AnyMap } from "./any-map";
 export type { AnyObject } from "./any-object";
 export type { EmptyObject } from "./empty-object";
 export type { Index } from "./object-index";
