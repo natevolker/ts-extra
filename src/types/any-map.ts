0cb4bda87e539afa1703c8dbf48b533891f200d5
Nate Volker <natevolker@gmail.com>
2022-01-24 15:50:47 -0600

    fix: adding AnyMap

---
+++ src/types/any-map.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/**
+ * Alias for `Map<any, any>`
+ *
+ * @typedef {Object} AnyMap
+ * @category Everyday
+ */
+export type AnyMap = Map<any, any>;
--- src/types/empty-object.ts
+++ src/types/empty-object.ts
@@ -1,2 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/ban-types
+/* eslint-disable @typescript-eslint/ban-types */
+
+/**
+ * Alias for `{}`
+ *
+ * @typedef {Object} EmptyObject
+ * @category Everyday
+ */
 export type EmptyObject = {};
