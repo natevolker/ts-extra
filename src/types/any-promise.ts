8820e0926164f071d72e112181f3c57653a48882
Nate Volker <natevolker@gmail.com>
2022-01-24 16:30:27 -0600

    fix: adding AnyPromise

---
+++ src/types/any-promise.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/**
+ * Alias for `Promise<any>`
+ *
+ * @typedef AnyPromise
+ * @category Everyday
+ */
+export type AnyPromise = Promise<any>;
